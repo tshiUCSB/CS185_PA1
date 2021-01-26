a3823ef175de8e06d6b099cc3daf8a84c6337480
Tina Shi <tshi@umail.ucsb.edu>
2021-01-26 15:37:13 -0800

    added scroll to top button

--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
-
 window.addEventListener('load', function() {
-	init_overlay("overlay", "grid-img");
 	init_scroll_top(25);
 
 });
--- js/video.js
+++ js/video.js
@@ -1,6 +1,6 @@
 
 window.addEventListener('load', function() {
-	init_overlay("overlay", "grid-img");
+	init_overlay("overlay", "grid-vid");
 	init_scroll_top(25);
 
 });
